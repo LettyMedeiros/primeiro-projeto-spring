b520a61834f35c0033aa8d13f1c4b54f47aa2907
Leticia Medeiros <leticiamedeiros@MacBook-Air-de-Leticia.local>
2019-02-06 21:05:41 -0400

    Arquivos iniciais do projeto

---
+++ src/main/java/com/cursoudemy/primeiroprojeto/PrimeiroprojetoApplication.java
@@ -0,0 +1,14 @@
+package com.cursoudemy.primeiroprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprojetoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/cursoudemy/primeiroprojeto/PrimeiroprojetoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cursoudemy.primeiroprojeto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimeiroprojetoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
